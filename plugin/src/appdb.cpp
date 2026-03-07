566b7c2583eff7b401bd231d2bafb2cb8a38d2f7
Rafael <rafael.aguiar02@gmail.com>
2026-03-07 15:02:00 -0300

    Cpp plugin and cmakelists for plugin

--- plugin/src/appdb.cpp
+++ plugin/src/appdb.cpp
@@ -1,1 +1,275 @@
 #include "appdb.h"
+
+#include <qobject.h>
+#include <qsqldatabase.h>
+#include <qsqlquery.h>
+#include <quuid.h>
+#include <utility>
+
+namespace myqmlplugin {
+AppEntry::AppEntry(QObject *entry, unsigned int frequency, QObject *parent)
+    : QObject(parent), m_entry(entry), m_frequency(frequency) {
+  const auto mo = m_entry->metaObject();
+  const auto tmo = metaObject();
+
+  // Shadowing quickshell's AppEntry props
+  for (const auto &prop : {"name", "comment", "execString", "categories"}) {
+    const auto metaProp = mo->property(mo->indexOfProperty(prop));
+    const auto thisMetaProp = tmo->property(tmo->indexOfProperty(prop));
+    QObject::connect(m_entry, metaProp.notifySignal(), this,
+                     thisMetaProp.notifySignal());
+  }
+  QObject::connect(m_entry, &QObject::destroyed, this, [this]() {
+    m_entry = nullptr;
+    deleteLater();
+  });
+}
+
+QObject *AppEntry::entry() const { return m_entry; }
+
+quint32 AppEntry::frequency() const { return m_frequency; }
+
+void AppEntry::setFrequency(unsigned int frequency) {
+  if (m_frequency != frequency) {
+    m_frequency = frequency;
+    emit frequencyChanged();
+  }
+}
+
+void AppEntry::incrementFrequency() {
+  ++m_frequency;
+  emit frequencyChanged();
+}
+
+QString AppEntry::id() const {
+  if (!m_entry) {
+    return "";
+  }
+  return m_entry->property("id").toString();
+}
+
+QString AppEntry::name() const {
+  if (!m_entry) {
+    return "";
+  }
+  return m_entry->property("comment").toString();
+}
+
+QString AppEntry::comment() const {
+  if (!m_entry) {
+    return "";
+  }
+  return m_entry->property("comment").toString();
+}
+
+QString AppEntry::execString() const {
+  if (!m_entry) {
+    return "";
+  }
+  return m_entry->property("execString").toString();
+}
+
+QString AppEntry::categories() const {
+  if (!m_entry) {
+    return "";
+  }
+  return m_entry->property("execString").toStringList().join(" ");
+}
+
+AppDb::AppDb(QObject *parent)
+    : QObject(parent), m_uuid(QUuid::createUuid().toString()) {
+  auto db = QSqlDatabase::addDatabase("QSQLITE", m_uuid);
+  db.setDatabaseName(":memory:");
+  db.open();
+
+  QSqlQuery query(db);
+  query.exec("CREATE TABLE IF NOT EXISTS \
+      frequencies (id TEXT PRIMARY KEY, frequency INTEGER)");
+}
+
+QString AppDb::uuid() const { return m_uuid; }
+
+QString AppDb::path() const { return m_path; }
+
+void AppDb::setPath(const QString &path) {
+  auto newPath = path.isEmpty() ? ":memory:" : path;
+
+  if (m_path == newPath) {
+    return;
+  }
+
+  m_path = newPath;
+  emit pathChanged();
+
+  auto db = QSqlDatabase::database(m_uuid, false);
+  db.close();
+  db.setDatabaseName(newPath);
+  db.open();
+
+  QSqlQuery query(db);
+  query.exec("CREATE TABLE IF NOT EXISTS \
+      frequencies (id TEXT PRIMARY KEY, frequency INTEGER)");
+
+  updateAppFrequencies();
+}
+
+QObjectList AppDb::entries() const { return m_entries; }
+
+void AppDb::setEntries(const QObjectList &entries) {
+  if (m_entries == entries) {
+    return;
+  }
+
+  m_entries = entries;
+  emit entriesChanged();
+}
+
+QStringList AppDb::favoriteApps() const { return m_favoriteApps; }
+
+void AppDb::setFavoriteApps(const QStringList &favoriteApps) {
+  if (m_favoriteApps == favoriteApps) {
+    return;
+  }
+
+  m_favoriteApps = favoriteApps;
+  emit favoriteAppsChanged();
+
+  m_favoriteAppsRegex.clear();
+  m_favoriteAppsRegex.reserve(m_favoriteApps.size());
+  for (const QString &item : std::as_const(m_favoriteApps)) {
+    const QRegularExpression re(regexifyString(item));
+    if (re.isValid()) {
+      m_favoriteAppsRegex << re;
+    } else {
+      qWarning() << "AppDb::setFavoriteApps: Invalid regex: " << re.pattern();
+    }
+  }
+
+  emit appsChanged();
+}
+
+QString AppDb::regexifyString(const QString &original) const {
+  if (original.startsWith('^') && original.endsWith('$')) {
+    return original;
+  }
+
+  const QString escaped = QRegularExpression::escape(original);
+  return QStringLiteral("^%1$").arg(escaped);
+}
+
+QQmlListProperty<AppEntry> AppDb::apps() {
+  return QQmlListProperty<AppEntry>(this, &getSortedApps());
+}
+
+void AppDb::incrementFrequency(const QString &id) {
+  auto db = QSqlDatabase::database(m_uuid);
+  QSqlQuery query(db);
+
+  query.prepare("INSERT INTO frequencies (id, frequency) "
+                "VALUES (:id, 1) "
+                "ON CONFLICT (id) DO UPDATE SET frequency = frequency + 1");
+  query.bindValue(":id", id);
+  query.exec();
+
+  auto *app = m_apps.value(id);
+  if (app) {
+    const auto before = getSortedApps();
+
+    app->incrementFrequency();
+
+    if (before != getSortedApps()) {
+      emit appsChanged();
+    }
+  } else {
+    qWarning() << "AppDb::incrementFrequency could not find app with id " << id;
+  }
+}
+
+QList<AppEntry *> &AppDb::getSortedApps() const {
+  m_sortedApps = m_apps.values();
+  std::sort(m_sortedApps.begin(), m_sortedApps.end(),
+            [this](AppEntry *a, AppEntry *b) {
+              bool aIsFav = isFavorite(a);
+              bool bIsFav = isFavorite(b);
+              if (aIsFav != bIsFav) {
+                return aIsFav;
+              }
+              if (a->frequency() != b->frequency()) {
+                return a->frequency() > b->frequency();
+              }
+              return a->name().localeAwareCompare(b->name()) < 0;
+            });
+
+  return m_sortedApps;
+}
+
+bool AppDb::isFavorite(const AppEntry *app) const {
+  for (const QRegularExpression &re : m_favoriteAppsRegex) {
+    if (re.match(app->id()).hasMatch()) {
+      return true;
+    }
+  }
+  return false;
+}
+
+quint32 AppDb::getFrequency(const QString &id) const {
+  auto db = QSqlDatabase::database(m_uuid);
+  QSqlQuery query(db);
+
+  query.prepare("SELECT frequency FROM frequencies WHERE id = :id");
+  query.bindValue(":id", id);
+
+  if (query.exec() && query.next()) {
+    return query.value(0).toUInt();
+  }
+
+  return 0;
+}
+
+void AppDb::updateAppFrequencies() {
+  const auto before = getSortedApps();
+
+  for (auto *app : std::as_const(m_apps)) {
+    app->setFrequency(getFrequency(app->id()));
+  }
+
+  if (before != getSortedApps()) {
+    emit appsChanged();
+  }
+}
+
+void AppDb::updateApps() {
+  bool dirty = false;
+
+  for (const auto &entry : std::as_const(m_entries)) {
+    const auto id = entry->property("id").toString();
+    if (!m_apps.contains(id)) {
+      dirty = true;
+      auto *const newEntry = new AppEntry(entry, getFrequency(id), this);
+      QObject::connect(newEntry, &QObject::destroyed, this, [id, this]() {
+        if (m_apps.remove(id)) {
+          emit appsChanged();
+        }
+      });
+      m_apps.insert(id, newEntry);
+    }
+  }
+
+  QSet<QString> newIds;
+  for (const auto &entry : std::as_const(m_entries)) {
+    newIds.insert(entry->property("id").toString());
+  }
+
+  for (auto it = m_apps.keyBegin(); it != m_apps.keyEnd(); ++it) {
+    const auto &id = *it;
+    if (!newIds.contains(id)) {
+      dirty = true;
+      m_apps.take(id)->deleteLater();
+    }
+  }
+
+  if (dirty) {
+    emit appsChanged();
+  }
+}
+} // namespace myqmlplugin
--- plugin/src/appdb.h
+++ plugin/src/appdb.h
@@ -1,3 +1,111 @@
 #pragma once
 
-namespace myplugin {}
+#include <qcontainerfwd.h>
+#include <qhash.h>
+#include <qobject.h>
+#include <qqmlintegration.h>
+#include <qqmllist.h>
+#include <qregularexpression.h>
+#include <qtmetamacros.h>
+#include <qtypes.h>
+
+// Credits to the Caelestia shell
+// https://github.com/caelestia-dots/shell/tree/main
+
+namespace myqmlplugin {
+class AppEntry : public QObject {
+  Q_OBJECT
+  QML_ELEMENT
+  QML_UNCREATABLE("No no no")
+
+  Q_PROPERTY(QObject *entry READ entry CONSTANT)
+
+  Q_PROPERTY(quint32 frequency READ frequency NOTIFY frequencyChanged)
+  Q_PROPERTY(QString id READ id CONSTANT)
+  Q_PROPERTY(QString name READ name NOTIFY nameChanged)
+  Q_PROPERTY(QString comment READ comment NOTIFY commentChanged)
+  Q_PROPERTY(QString execString READ execString NOTIFY execStringChanged)
+  Q_PROPERTY(QString categories READ categories NOTIFY categoriesChanged)
+
+public:
+  explicit AppEntry(QObject *entry, quint32 frequency,
+                    QObject *parent = nullptr);
+
+  [[nodiscard]] QObject *entry() const;
+
+  [[nodiscard]] quint32 frequency() const;
+  void setFrequency(quint32 frequenct);
+  void incrementFrequency();
+
+  [[nodiscard]] QString id() const;
+  [[nodiscard]] QString name() const;
+  [[nodiscard]] QString comment() const;
+  [[nodiscard]] QString execString() const;
+  [[nodiscard]] QString categories() const;
+
+signals:
+  void frequencyChanged();
+  void nameChanged();
+  void commentChanged();
+  void execStringChanged();
+  void categoriesChanged();
+
+private:
+  QObject *m_entry;
+  quint32 m_frequency;
+};
+
+class AppDb : public QObject {
+  Q_OBJECT
+  QML_ELEMENT
+
+  Q_PROPERTY(QString uuid READ uuid CONSTANT)
+  Q_PROPERTY(QString path READ path WRITE setPath NOTIFY pathChanged REQUIRED)
+  Q_PROPERTY(QObjectList entries READ entries WRITE setEntries NOTIFY
+                 entriesChanged REQUIRED)
+  Q_PROPERTY(QStringList favoriteApps READ favoriteApps WRITE setFavoriteApps
+                 NOTIFY favoriteAppsChanged REQUIRED)
+  Q_PROPERTY(
+      QQmlListProperty<myqmlplugin::AppEntry> apps READ apps NOTIFY appsChanged)
+
+public:
+  explicit AppDb(QObject *parent = nullptr);
+
+  [[nodiscard]] QString uuid() const;
+
+  [[nodiscard]] QString path() const;
+  void setPath(const QString &path);
+
+  [[nodiscard]] QObjectList entries() const;
+  void setEntries(const QObjectList &entries);
+
+  [[nodiscard]] QStringList favoriteApps() const;
+  void setFavoriteApps(const QStringList &favoriteApps);
+
+  [[nodiscard]] QQmlListProperty<AppEntry> apps();
+
+  Q_INVOKABLE void incrementFrequency(const QString &id);
+
+signals:
+  void pathChanged();
+  void entriesChanged();
+  void favoriteAppsChanged();
+  void appsChanged();
+
+private:
+  const QString m_uuid;
+  QString m_path;
+  QObjectList m_entries;
+  QStringList m_favoriteApps;
+  QList<QRegularExpression> m_favoriteAppsRegex;
+  QHash<QString, AppEntry *> m_apps;
+  mutable QList<AppEntry *> m_sortedApps;
+
+  QString regexifyString(const QString &original) const;
+  void updateApps();
+  bool isFavorite(const AppEntry *app) const;
+  QList<AppEntry *> &getSortedApps() const;
+  quint32 getFrequency(const QString &id) const;
+  void updateAppFrequencies();
+};
+} // namespace myqmlplugin
